ce09d2b77f8d242f22c8a3c7635fcb22b78e114e
dunjunliu <dunjun.liu@gmail.com>
2013-07-31 21:21:00 +0800

    first commit

---
+++ src/com/and/netease/TabFollowActivity.java
@@ -0,0 +1,14 @@
+package com.and.netease;
+//Download by http://www.codefans.net
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TabFollowActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_follow);
+	}
+}
---
+++ src/com/and/netease/TabVoteActivity.java
@@ -0,0 +1,14 @@
+package com.and.netease;
+//Download by http://www.codefans.net
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TabVoteActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_vote);
+	}
+}
